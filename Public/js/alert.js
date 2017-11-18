fe7fd98eebf3c3055c835a85c8c987f2b80f5448
tangli <catchyouhand@qq.com>
2017-11-18 11:50:47 +0800

    修改邮箱

---
+++ Public/js/alert.js
@@ -0,0 +1,11 @@
+			$(".form_input_inner_btn.font_normal").on('click',function(){
+				$(".alert_box_div").removeClass("hidden");
+				var height=$(document).height();
+				$(".alert_box_div").height(height);
+			});
+			$(".alert_box_inner .header .right span").on("click",function(){
+				$(".alert_box_div").addClass("hidden");
+			});
+			$(".alert_close").on("click",function(){
+				$(".alert_box_div").addClass("hidden");
+			});
