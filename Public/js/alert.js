c035ad5a0433231a605049e16448436e552e385d
tangli <catchyouhand@qq.com>
2017-11-19 18:35:19 +0800

    解绑
    解绑操作

--- Public/js/alert.js
+++ Public/js/alert.js
@@ -1,7 +1,5 @@
 			$(".form_input_inner_btn.font_normal").on('click',function(){
-				$(".alert_box_div").removeClass("hidden");
-				var height=$(document).height();
-				$(".alert_box_div").height(height);
+				show_alert();
 			});
 			$(".alert_box_inner .header .right span").on("click",function(){
 				$(".alert_box_div").addClass("hidden");
@@ -9,3 +7,8 @@
 			$(".alert_close").on("click",function(){
 				$(".alert_box_div").addClass("hidden");
 			});
+			function show_alert(){
+				$(".alert_box_div").removeClass("hidden");
+				var height=$(document).height();
+				$(".alert_box_div").height(height);
+			}
